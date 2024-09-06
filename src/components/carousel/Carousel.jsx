db6e849587d81ced75b4853a8b7d63fb26d71788
Bolortox <bolortogosboujee@gmail.com>
2024-09-06 21:17:24 +0800

    carousel and homepage changed

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,9 +1,22 @@
 import { LeftIcon } from "../svg/LeftIcon";
 import { RightIcon } from "../svg/RightIcon";
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Carousel = ({ slides = [] }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [carouselData, setCarouselData] = useState([]);
+
+  const fetchCarouselData = () => {
+    fetch(`https://dev.to/api/articles?per_page=6&top=6`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCarouselData(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchCarouselData();
+  }, []);
 
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
@@ -20,18 +33,20 @@ export const Carousel = ({ slides = [] }) => {
   return (
     <div className=" h-[800px] cover w-full m-auto py-16 relative pt-28 ">
       <div
-        style={{ backgroundImage: `url(${slides[currentIndex]?.cover_image})` }}
+        style={{
+          backgroundImage: `url(${carouselData[currentIndex]?.cover_image})`,
+        }}
         className=" p-3 w-full h-full rounded-2xl bg-center bg-cover duration-500 "
       >
         <div className=" w-[598px] h-[286px] absolute bottom-20  p-10 bg-white rounded-xl flex flex-col gap-6">
           <button className="w-[97px] h-[28px] bg-indigo-500 text-white flex justify-center items-center rounded-md text-xs  non-italicfont-medium hover:shadow-2xl ">
-            Technology
+            {carouselData[currentIndex]?.tag_list[0]}
           </button>
           <div className="text-4xl font-semibold font-sans">
-            Grid system for better Design User Interface
+            {carouselData[currentIndex]?.title}
           </div>
           <div className="text-sm font-sans text-slate-400 ">
-            August 20, 2022
+            {carouselData[currentIndex]?.published_at}
           </div>
         </div>
       </div>
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -8,18 +8,21 @@ import { Trending } from "../trending/Trending";
 export default function HomePage() {
   const [articles, setArticles] = useState([]);
   const [trendingData, setTrendingData] = useState([]);
+  const [contentNumber, setContentNumber] = useState(9);
 
   const [tag, setTag] = useState();
+
+  const fetchButtonData = () => {
+    fetch(`https://dev.to/api/articles?per_page=${contentNumber}`)
+      .then((response) => response.json())
+      .then((data) => setArticles(data));
+  };
 
   const fetchData = () => {
     fetch(`https://dev.to/api/articles?tag=${tag}`)
       .then((response) => response.json())
       .then((data) => setArticles(data));
   };
-
-  useEffect(() => {
-    fetchData();
-  }, [tag]);
 
   const fetchTrendingData = () => {
     fetch(`https://dev.to/api/articles?per_page=4&top=4`)
@@ -33,15 +36,36 @@ export default function HomePage() {
   };
 
   useEffect(() => {
+    fetchData();
+  }, [tag]);
+
+  useEffect(() => {
     fetchTrendingData();
   }, []);
+
+  useEffect(() => {
+    fetchButtonData();
+  }, [contentNumber]);
+
+  const handleClickLoadMore = () => {
+    setContentNumber(contentNumber + 3);
+  };
+
   return (
     <div className="w-full h-full m-auto">
       <div className="container flex flex-col max-w-[1216px] w-auto h-auto m-auto ">
         <Header />
-        <Carousel slides={articles} />
+        <Carousel />
         <Trending trendingData={trendingData} />
         <BlogPost filterTag={filterTag} articles={articles} />
+        <div className="flex justify-center items-center py-5">
+          <button
+            onClick={handleClickLoadMore}
+            className="w-[123px] h-[48px] border border-slate-300 rounded-md"
+          >
+            Load More
+          </button>
+        </div>
         <div className="bg-slate-50">
           <Footer />
         </div>
